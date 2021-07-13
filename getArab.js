271cc5461d04c724b8921a7c0231ad6a393de45a
Norman Gao <1311167847@qq.com>
2021-07-13 16:59:56 +0800

    fix bug

--- getArab.js
+++ getArab.js
@@ -1,8 +1,7 @@
 let number = {"壹": 1, "一": 1, "贰": 2, "二": 2,"两":2, "叁": 3, "三": 3, "肆": 4, "四": 4, "伍": 5, "五": 5, "陆": 6, "六": 6, "柒": 7, "七": 7, "捌": 8, "八": 8, "玖": 9, "九": 9, "零": 0}
 let times = {"拾":10, "十":10, "佰":100, "百":100, "仟":1000, "千":1000, "万":10000, "亿":100000000, "兆":1000000000000, "京":10000000000000000}
 
-function splitStr(s) {
-    let sum = 0
+function splitStr(s, sum = 0) {
     let status = 0
     for (let i in s) {
         if (s[i] == "万" || s[i] == "亿" || s[i] == "兆" || s[i] == "京") {
@@ -11,7 +10,7 @@ function splitStr(s) {
             let res = handleNumber(head)
             sum += res * times[s[i]]
             status = 1
-            return splitStr(tail)
+            return splitStr(tail, sum)
         }
     }
     if (status == 0 && s.length > 0) {
